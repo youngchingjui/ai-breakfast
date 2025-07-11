d77c7d3a727a81e9444d27547356d4fb472abc04
Ching Jui Young <young.chingjui@gmail.com>
2025-07-11 13:18:32 +0800

    fine

---
+++ nextjs/app/page.tsx
@@ -0,0 +1,9 @@
+import AIBreakfastPoster from "../ai-breakfast-poster"
+
+export default function Page() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-4">
+      <AIBreakfastPoster />
+    </div>
+  )
+}
